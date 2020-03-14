15c5d753b22f83e1eb6eb398e84e3683fe133b2c
intodar <slavaguzev1@gmail.com>
2020-03-14 23:15:11 +0300

    Update talbotmatrix

--- source/talbotmatrix.cpp
+++ source/talbotmatrix.cpp
@@ -1,6 +1,65 @@
 #include "talbotmatrix.h"
 
+#include <cmath>
+#include <algorithm>
+
+#define DEFAULT_SCALE 1
+
+#define DEFAULT_WIDTH 1024
+#define DEFAULT_HEIGHT 768
+
+using namespace std;
+
 TalbotMatrix::TalbotMatrix()
 {
+    this->width = DEFAULT_WIDTH;
+    this->height = DEFAULT_HEIGHT;
+	this->generated = false;
+}
 
+TalbotMatrix::TalbotMatrix(unsigned int _width, unsigned int _height):TalbotMatrix()
+{
+    this->width = _width;
+    this->height = _height;
+	this->generateMatrix();
+}
+
+vector<pair<unsigned int, unsigned int>> TalbotMatrix::placeSources(void)
+{
+    vector<pair<unsigned int, unsigned int>> sources;
+    if (this->sources_quantity > 0)
+    {
+        size_t placed = 0;
+        double step = this->height / (this->sources_quantity + 1);
+        for (double iteration = 0; iteration < this->height; iteration += step)
+        {
+            if (placed == this->sources_quantity)
+                break;
+            sources.push_back({0, iteration});
+            placed++;
+        }
+    }
+    return sources;
+}
+
+float TalbotMatrix::amplitudeFunction(int x, int y, size_t time)
+{
+    float distance = sqrt(pow(x, 2) + pow(y, 2)) + time;
+    float function_value = this->max_amplitude * pow((sin(distance) + 1) / 2, this->wave_slope);
+    return function_value;
+}
+
+void TalbotMatrix::generateMatrix()
+{
+	if (this->generated)
+		return;
+
+    auto sources = this->placeSources();
+
+    unsigned int wave_distance = max(this->height, this->width); //Дистанция, на которую волна распространяется
+
+    for (auto &i : sources)
+    {
+        for (int i = )
+    }
 }
--- source/talbotmatrix.h
+++ source/talbotmatrix.h
@@ -1,12 +1,62 @@
 #ifndef TALBOTMATRIX_H
 #define TALBOTMATRIX_H
 
+#include <vector>
 #include <QPixmap>
+
+using namespace std;
 
 class TalbotMatrix : public QPixmap
 {
+private:
+	vector<vector<unsigned short>> matrix;
+
+	bool generated;
+	unsigned int width;
+	unsigned int height;
+	// остальные параметры ковра (прокомментировать)
+
+	// Количество источников
+	unsigned int sources_quantity;
+
+    // Амплитуда колебаний источника
+    int max_amplitude;
+
+	// Фаза волны
+	double phase;
+
+	// Длина волны
+	double wave_len;
+
+    // Крутость волны (крутость откоса)
+    double wave_slope;
+
+	// ****************************************
+	// переменные всякие дополнительные
+    // функции всякие дополнительные
+	// ****************************************
+
+    // Функция размещения источников на генерируемой плоскости
+    vector<pair<unsigned int, unsigned int>> placeSources(void);
+
+    // Функция рассчёта амплитуды волны
+    float amplitudeFunction(int x, int y, size_t time);
+
+	// генерирует необходимую матрицу и сохраняет результат в matrix (без аргументов)
+	// присваивает generated true;
+	void generateMatrix(void);
+
 public:
 	TalbotMatrix();
+	TalbotMatrix(unsigned int _width, unsigned int _height); // добавь в аргументы остальные параметры ковра
+
+	void updateParams(void); // добавь в аргументы остальные параметры ковра
+	void updateSize(unsigned int _width, unsigned int _height); // обновляет размер окна
+	void updateSources(unsigned int sources_quantity); //
+
+	vector<vector<unsigned short> > getMatrix(); // генерирует картинку с помощью generateMatrix и возвращает матрицу
+
+	~TalbotMatrix();
 };
 
 #endif // TALBOTMATRIX_H
