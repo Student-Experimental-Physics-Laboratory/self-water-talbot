829398cfd421fc51c099825fd29bee83c796eaff
Paul <tscheklin@gmail.com>
2020-03-28 12:47:15 +0300

    Initial added

---
+++ source/drawchart.cpp
@@ -0,0 +1,6 @@
+#include "drawchart.h"
+
+DrawChart::DrawChart(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ source/drawchart.h
@@ -0,0 +1,16 @@
+#ifndef DRAWCHART_H
+#define DRAWCHART_H
+
+#include <QWidget>
+
+class DrawChart : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DrawChart(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // DRAWCHART_H
