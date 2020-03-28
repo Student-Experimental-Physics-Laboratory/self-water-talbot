2d8978cf0dc4bfad344b4517e970555d7fdf83e2
Vyacheslav Guzev <slavaguzev1@gmail.com>
2020-03-28 09:58:36 +0300

    Update mainwindow.cpp

--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "talbotmatrix.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,11 +14,16 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::on_drawButton_clicked()
 {
-    // добавляй сюда свою функцию
-    ui->widget->drawVector(
-                std::vector<std::vector<unsigned int>>
-                (100, std::vector<unsigned int>(100, 100)));
+    int sources = this->ui->sources_input->value();
+    double phase = this->ui->phase_input->value();
+    double wave_len = this->ui->wavelen_input->value();
+    double wave_slope = this->ui->slope_input->value();
+    bool is_reflectible = this->ui->is_reflectible_input->isChecked();
+    double viscosity = this->ui->viscosity_input->value();
+    TalbotMatrix buf_mat(this->ui->widget->size().height(), this->ui->widget->size().width());
+    buf_mat.updateSources(sources, is_reflectible);
+    buf_mat.updateParams(phase, wave_len, wave_slope, viscosity);
+    ui->widget->drawVector(buf_mat.getMatrix());
 }
