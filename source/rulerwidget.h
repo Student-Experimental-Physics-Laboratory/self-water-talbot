862d43b5103abd2eceb24d58954163b1d1271d63
Paul <tscheklin@gmail.com>
2020-08-01 15:01:07 +0300

    Hello Slava I added some nice things for your ruler, you can say thank you to me, if, of course, you can do it, now good bye:

--- source/rulerwidget.h
+++ source/rulerwidget.h
@@ -1,11 +1,31 @@
 #ifndef RULERWIDGET_H
 #define RULERWIDGET_H
 
+#include "imagewidget.h"
+#include "imageprocesser.h"
+
+// In this shit you store all your parameters (for example
+// vertical/horizontal ruler
+class RulerProcesser : public ImageProcesser
+{
+public:
+   void process(QImage &image) override; // TODO implement
+};
+
 
 class RulerWidget : public ImageWidget
 {
 public:
     RulerWidget();
+    void reprocess() override; //  TODO here just use process(QImage) from your
+    // processer
+
+protected:
+    void paintEvent(QPaintEvent *event) override; // TODO just draw image
+    // (image is in base class)
+
+private:
+    RulerProcesser processer;
 };
 
 #endif // RULERWIDGET_H
